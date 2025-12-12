9ce9e65a3a4356ba4e2a6d73b267441167d03981
malya srivastava <malyasri0112@gmail.com>
2025-12-12 23:54:32 +0530

    fixed responsiveness

--- bentofolio/src/App.jsx
+++ bentofolio/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
 import Header from './components/Header';
 import Home from './pages/Home';
 import ProjectsPage from './pages/ProjectsPage';
@@ -12,7 +11,7 @@ const App = () => {
     <Router>
       <div className="min-h-screen bg-zinc-900 text-white flex flex-col">
         <Header />
-        <div className="mx-8 mb-12 mt-12 flex-grow">
+        <div className="mx-4 sm:mx-8 mb-12 mt-12 flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/dev-projects" element={<ProjectsPage />} />
